e0b0f1db748fd18afe7386367520bd74976c07c5
josiah <aideloje.josiah@lmu.edu.ng>
2025-07-16 01:48:53 +0100

    feat: recreate sitemap.js for dynamic sitemap generation

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://josiah-portfolio1.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
